85d39abc13313516be5126a3f38b7180c1bba0b4
Alex <abramov231@gmail.com>
2018-10-26 02:06:45 +0300

    child_idea

--- server/orm/idea.ts
+++ server/orm/idea.ts
@@ -1,12 +1,64 @@
 
-import { prop, Typegoose, ModelType, InstanceType } from 'typegoose'
+import { prop, pre, Typegoose, ModelType, InstanceType } from 'typegoose'
 import * as mongoose from 'mongoose'
 
+@pre<Idea>('save', function(next) { // or @pre(this: Car, 'save', ...
+    console.log('Pre save', this.title, this.id, this.parentIdea, this.comments);
+    if (this.parentIdea) {
+        IdeaModel.update({
+            _id: this.parentIdea
+        }, {
+            $push: {
+                comments: this._id
+            }
+        }, next);
+    } else {
+        next();
+    }
+})
 export class Idea extends Typegoose {
     id: any;
 
     @prop()
+    type: string;
+    @prop()
     title: string;
+    @prop()
+    description: string;
+    @prop()
+    details: string;
+    @prop()
+    attachements: Array<Object>;
+
+    @prop({})
+    author: mongoose.Types.ObjectId;
+    @prop({ref: 'Idea'})
+    parentIdea: mongoose.Types.ObjectId;
+
+    @prop()
+    voteType: string;
+    @prop()
+    availableActions: Array<string>;
+
+    @prop()
+    votesPlus: Array<string>;
+    @prop()
+    votesMinus: Array<string>;
+    @prop()
+    skips: Array<string>;
+    @prop()
+    views: Array<string>;
+
+    @prop({ref: 'Idea', default: []})
+    ideasPlus: Array<mongoose.Types.ObjectId>;
+    @prop({ref: 'Idea', default: []})
+    ideasMinus: Array<mongoose.Types.ObjectId>;
+    @prop({ref: 'Idea', default: []})
+    alternatives: Array<mongoose.Types.ObjectId>;
+    @prop({ref: 'Idea', default: []})
+    comments: Array<mongoose.Types.ObjectId>;
 }
 
-export default new Idea().getModelForClass(Idea);
+const IdeaModel = new Idea().getModelForClass(Idea);
+
+export default IdeaModel;
--- server/tests/orm/index.test.ts
+++ server/tests/orm/index.test.ts
@@ -7,6 +7,7 @@ test('connect', async () => {
     const orm = new ORM(config.mongoose);
     const db = await orm.connect();
     expect(db).toBe(mongoose);
+    await mongoose.connection.close();
 });
 
 describe('Idea', () => {
@@ -18,10 +19,14 @@ describe('Idea', () => {
     });
 
     beforeEach(async () => {
-        await ORM.Idea.collection.drop();
+        await mongoose.connection.db.dropDatabase();
     });
 
-    test('save idea', async () => {
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+
+    test('save root idea', async () => {
         const idea = new ORM.Idea({
             title: 'Idea title',
         });
@@ -31,5 +36,28 @@ describe('Idea', () => {
         await idea.save();
         
         expect(await ORM.Idea.countDocuments()).toBe(1);
+    });
+
+    test('save child idea', async () => {
+        expect(await ORM.Idea.countDocuments()).toBe(0);
+
+        const rootIdea = new ORM.Idea({
+            title: 'Root idea title',
+        });
+        await rootIdea.save();
+
+        const idea = new ORM.Idea({
+            title: 'Child idea title',
+            parentIdea: rootIdea._id,
+        });
+        await idea.save();
+    
+        expect(await ORM.Idea.countDocuments()).toBe(2);
+
+        const actualRootIdea = await ORM.Idea.findById(rootIdea._id);
+        console.log(actualRootIdea);
+
+        expect(actualRootIdea.title).toBe(rootIdea.title);
+        expect(actualRootIdea.comments).toEqual([idea._id]);
     });
 });
