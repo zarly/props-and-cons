cd9603ea58ec136817f7b445536e3804adc5b4b5
Alex <abramov231@gmail.com>
2018-11-03 03:31:11 +0300

    deploy

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,11 @@
 
+import * as dotenv from 'dotenv'
 import config from './config'
 import Logic from './logic'
 import ORM from './orm'
 import Server from './server'
+
+dotenv.config();
 
 const orm = new ORM(config.mongoose);
 const logic = new Logic(orm);
