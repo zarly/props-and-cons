617fe98f8c9232e4b58055453df1d756e72c596c
Alexander Abramov <alabr@yandex-team.ru>
2018-11-13 09:56:13 +0300

    idea votes ui

--- client/src/pages/idea.vue
+++ client/src/pages/idea.vue
@@ -2,14 +2,53 @@
 	<div class="IdeaPage VkPage">
 		<div class="anch" @click="$router.push('/ideas')">к списку тем</div>
 		<div v-if="idea">
-			<h2 v-text="idea.title"></h2>
-			<p v-text="idea.description"></p>
+			<div class="title" v-text="idea.title"></div>
+			<div class="description" v-text="idea.description"></div>
+			<div class="created-area">
+				<a v-text="idea.author.name" class="anch" :href="authorUrl" target="_blank"></a>,
+				<span v-text="datetime" class="hint"></span>
+			</div>
+			<div class="actions row">
+				<button @click="vote(3)" class="option">
+					Голосовать За
+				</button>
+				<button @click="vote(4)" class="option">
+					Голосовать Против
+				</button>
+				<button @click="vote(2)" class="option">
+					Пропустить
+				</button>
+			</div>
+			<div class="stats row">
+				<div class="stat-box">
+					<span class="count" v-text="idea.votesPlus"></span>
+					<div class="label">поддержали</div>
+				</div>
+				<div class="stat-box">
+					<span class="count" v-text="idea.votesMinus"></span>
+					<div class="label">возразили</div>
+				</div>
+				<div class="stat-box">
+					<span class="count" v-text="idea.skips"></span>
+					<div class="label">пропустили</div>
+				</div>
+				<div class="stat-box">
+					<span class="count" v-text="acceptance"></span>
+					<div class="label">одобрение</div>
+				</div>
+				<div class="stat-box">
+					<span class="count" v-text="relevance"></span>
+					<div class="label">актуальность</div>
+				</div>
+			</div>
+			<!--<div v-text="idea"></div>-->
 		</div>
 	</div>
 </template>
 
 <script>
 	import gate from '../modules/gate'
+	import {renderDatetime} from '../modules/decorators'
 
 	export default {
 		props: ['id'],
@@ -19,17 +58,101 @@
 				idea: null,
 			};
 		},
+		computed: {
+			datetime () {
+				const date = new Date(this.idea.createdAt);
+				return renderDatetime(date);
+			},
+			authorUrl () {
+				return `https://vk.com/id${this.idea.author.vkUid}`
+			},
+			acceptance () {
+				const {idea} = this;
+				const val = idea.votesPlus / (idea.votesPlus + idea.votesMinus);
+				if (val >= 0) {
+					return Math.round(100 * val) + '%';
+				} else {
+					return '-';
+				}
+			},
+			relevance () {
+				const {idea} = this;
+				const val = (idea.votesPlus + idea.votesMinus) / (idea.votesPlus + idea.votesMinus + idea.skips);
+				if (val >= 0) {
+					return Math.round(100 * val) + '%';
+				} else {
+					return '-';
+				}
+			}
+		},
 		methods: {
 			async fetch () {
 				this.idea = await gate.ask(`/ideas/${this.id}`);
-			}
+			},
+			async vote (voteType) {
+				const query = {
+					ideaId: this.idea._id,
+					voteType,
+				};
+				await gate.ask('/vote', {
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+					},
+					method: 'POST',
+					body: JSON.stringify(query),
+				});
+				await this.fetch();
+			},
 		},
 	};
 </script>
 
 <style scoped lang="less">
-	@import-once "../styles/variables";
+	@import "../styles/variables";
 
 	.IdeaPage {
+		.title {
+			margin-top: 10px;
+			font-size: 15px;
+			font-weight: 500;
+		}
+
+		.description {
+			margin-top: 10px;
+		}
+
+		.created-area {
+			margin-top: 10px;
+			text-align: right;
+			color: @cl-grey;
+
+			.anch {
+				font-weight: normal;
+				color: @cl-grey;
+			}
+		}
+
+		.actions {
+			margin-top: 10px;
+
+			.option {
+				width: 30%;
+			}
+		}
+
+		.stats {
+			margin-top: 10px;
+
+			.stat-box {
+				width: 100px;
+				height: 100px;
+
+				.count {
+					font-size: 30px;
+					text-align: right;
+				}
+			}
+		}
 	}
 </style>
