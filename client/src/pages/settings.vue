62c1bcf8e3306f38da8f8efe83b20aa9eaa22d83
Alexander Abramov <alabr@yandex-team.ru>
2018-12-08 16:28:25 +0300

    fix settings

--- client/src/pages/settings.vue
+++ client/src/pages/settings.vue
@@ -2,10 +2,17 @@
 	<div class="SettingsPage VkPage">
 		<div class="anch" @click="$router.push('/ideas')">&lt; список тем</div>
 		<h1>Настройки</h1>
-		<div v-if="me && me.vkInfo" class="user-card">
+		<!--div v-if="me && me.vkInfo" class="user-card">
 			<img class="user-photo" :src="me.vkInfo.photo_100"/>
 			<a class="user-name anch" v-text="me.vkInfo.first_name + ' ' + me.vkInfo.last_name" :href="'https://vk.com/id' + me.vkInfo.id"></a>
-		</div>
+		</div-->
+		<p>
+			Приветствуем, <span v-text="me.vkInfo.first_name"></span>! Это страница администратора, пока она пуста.
+			Вы можете продложить необходимые фичи на странице обсуждения приложения, по кнопке ниже.
+		</p>
+		<a href="https://vk.com/app6210627_-174087331?ad_id=app_settings" target="_top">
+			<button>Запросить новые фичи</button>
+		</a>
 		<!--<div>{{me}}</div>-->
 		<!--<div>{{group}}</div>-->
 		<!--<div>{{global}}</div>-->
