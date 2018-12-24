49eff23a4655122dd2f806198dfff6dccdc10047
Alexander Abramov <alabr@yandex-team.ru>
2018-12-24 21:08:42 +0300

    ssr

---
+++ tasks/render_static_client/index.js
@@ -0,0 +1,23 @@
+
+// Шаг 1: Создаём экземпляр Vue
+const Vue = require('vue');
+const app = new Vue({
+	template: `<div>Hello World</div>`
+});
+
+// Шаг 2: Создаём рендерер
+const renderer = require('vue-server-renderer').createRenderer();
+
+// // Шаг 3: Рендерим экземпляр Vue в HTML
+// renderer.renderToString(app, (err, html) => {
+// 	if (err) throw err;
+// 	console.log(html);
+// 	// => <div data-server-rendered="true">Hello World</div>
+// });
+
+// с версии 2.5.0+, возвращает Promise если коллбэк не указан:
+renderer.renderToString(app).then(html => {
+	console.log(html);
+}).catch(err => {
+	console.error(err);
+});
